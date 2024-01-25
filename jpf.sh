e064d65078f0bf2d81b984696b203ed8ac079392
e205723 <e205723@ie.u-ryukyu.ac.jp>
2024-01-26 05:09:13 +0900

    fix: RunJPF.jar not found

--- jpf.sh
+++ jpf.sh
@@ -3,10 +3,8 @@
 # unix shell script to run jpf
 #
 
-JPF_HOME=`dirname "$0"`/..
-
 if test -z "$JVM_FLAGS"; then
   JVM_FLAGS="-Xmx10g -ea"
 fi
 
-java $JVM_FLAGS -jar "$JPF_HOME/build/RunJPF.jar" "$@"
+java $JVM_FLAGS -jar "/opt/jpf-core/build/RunJPF.jar" "$@"
